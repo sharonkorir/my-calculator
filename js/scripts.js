43a27bc63c762e7c8867b1ecd95fe781c6c9f867
Sharon-Korir <sharon.korir@student.moringaschool.com>
2022-01-01 23:49:10 +0300

    seperation of logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//business logic
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -10,8 +12,15 @@ var divide= function(number1, number2){
 var multiply= function(number1, number2){
   return number1*number2;
 }
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
 
+//user interface logic
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#output").text(result);
+  });
+  });
